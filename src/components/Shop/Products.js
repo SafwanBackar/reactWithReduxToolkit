c51dd8b75de181d54f6853c11bb9a46b062dbd0e
Safwan Backar <safwanbackar29@gmail.com>
2022-07-15 18:36:18 +0530

    basic cart made with redux toolkit

---
+++ src/components/Shop/Products.js
@@ -0,0 +1,39 @@
+import ProductItem from './ProductItem';
+import classes from './Products.module.css';
+
+const Products = (props) => {
+
+  const DUMMY_PRODUCTS = [
+    {
+      id: 'p1',
+      price: 6,
+      title: "My first Book",
+      description: 'This is my first book'
+    },
+    {
+      id: 'p2',
+      price: 5,
+      title: "My second Book",
+      description: 'This is my second book'
+    }
+  ]
+
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCTS.map(product =>(
+        <ProductItem
+          id={product.id}
+          key = {product.id}
+          title={product.title}
+          price={product.price}
+          description={product.description}
+        />
+      ))}
+      </ul>
+    </section>
+  );
+};
+ 
+export default Products;
